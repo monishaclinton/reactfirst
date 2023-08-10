a37fbbd6e6a8a1db4f9f142b2ab7a29605f552bd
monishaclinton <monishajustinn97@gmail.com>
2023-08-09 19:09:55 -0700

    chats

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,46 +8,61 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { BrowserRouter as Router, Route, Link as Rlink, Routes, BrowserRouter,useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link as Rlink, Routes, BrowserRouter, useNavigate } from "react-router-dom";
 import Register from './register';
 import asserts from '../asserts/cupid-heart.jpg'
 import Chatcontainer from '../components/chatcontainer';
+import Axios from 'axios';
 
+
+const server_base_url = "http://localhost:3001/";
 
 export default function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-  let navigate = useNavigate(); 
-  const routeToChatbox = () =>{ 
-   
-    navigate("/chatcontainer");
-  };
-  return (
-   
-    <div>
-     
- 
-  
-      <Container id="Main"maxWidth="xs">
+    // console.log({
+    //   email: data.get('email'),
+    //   password: data.get('password'),
+    // });
 
-        
+    Axios.post(server_base_url + "get_user_row", data, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      console.log(response.data)
+
+      if (response.data[0]) {
+        alert("Logined As : " + response.data[0].user_firstname + response.data[0].user_lastname);
+        window.location.replace('http://localhost:3000/chatcontainer');
+      }else{
+        alert('Invalid Username or Password')
+      }
+    });
+
+  };
+
+  return (
+
+    <div>
+
+
+
+      <Container id="Main" maxWidth="xs">
+
+
         <Box sx={{ marginTop: 20, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
 
-           <img src={require('../asserts/cupid-heart.jpg')} alt="logo" width={150} height={150}/>
+          <img src={require('../asserts/cupid-heart.jpg')} alt="logo" width={150} height={150} />
           <Box component="form" onSubmit={handleSubmit} >
-            <TextField margin="normal" fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus  required/>
+            <TextField margin="normal" fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus required />
             <TextField margin="normal" fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" required />
             <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" /><br />
             <Grid container spacing={2}><Grid item xs > <Button type="submit" variant="outlined"> Login with OTP </Button></Grid>
-              <Grid item ><Button type="submit" variant="contained" onClick={routeToChatbox} target="_blank"> Log In </Button></Grid></Grid>
+              <Grid item ><Button type="submit" variant="contained"> Log In </Button></Grid></Grid>
             <Box sx={{ display: 'flex', flexDirection: 'row-reverse', marginTop: 1 }}> <Link href="/Register" > Register here </Link></Box>
-             
+
           </Box>
         </Box>
 
--- src/pages/register.js
+++ src/pages/register.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useState} from 'react';
+import { useState } from 'react';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -11,61 +11,71 @@ import InputMask from 'react-input-mask';
 import Axios from 'axios';
 
 
+const server_base_url = "http://localhost:3001/";
+function Register() {
+    // const [value, setValue] = useState("");
+    // const handleChange = (e) => {
+    //     setValue = (e.target.value);
+    //     console.log(e.target.value);
+    // };
 
-function Register () {
-  const [value, setValue]= useState("");
-  const handleChange = (e) => {
-  setValue=(e.target.value);
-  console.log(e.target.value);
- };
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    //  Axios.post("http://localhost:3001/user/register", data)
-    //   .then((response) => {
+    const handleSubmit = (event) => {
+        event.preventDefault(); //ithu form submit aagi another page ku pooratha stop pannum
+        // alert()
+        const data = new FormData(event.currentTarget); // to get all form datas
+        Axios.post(server_base_url + "register_user", data, {   //it is use to sent the data to another server ..similar to ajax without page loading to push data in data
+            headers: {
+                "Content-Type": "application/json"    //its converts the data to json data and sent to mentioned url 
+            }
+        })
+            .then((response) => {    //response get the data from express server returned data
+                // console.log(response.data)
+                if (response.data == "success") {
+                    alert("User Successfully Added")
+                    window.location.replace('http://localhost:3000/');
+                } else {
+                    // setIsFormInvalid(true) 
+                }
 
-    //     if (response.data.status == "success") {
-    //       window.location.replace('http://localhost:3000/');
-    //     } else {
-    //       setIsFormInvalid(true)
-    //     }
-    //   });
+            });
 
-  };
- 
-    
-
-  return (
-    <div>
-
-      <Container maxWidth="xs">
-
-
-        <Box sx={{ marginTop: 20, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
-
-
-          <Box component="form" onSubmit={handleSubmit} >
-            <TextField margin="normal" value={value} fullWidth id="email" label="Your Email Address" name="email" autoComplete="email"   autoFocus  required onChange={handleChange}/>
-            <TextField margin="normal" fullWidth name="password" label="Your Password"  type="password" id="password"  required/>
-            <TextField margin="normal" fullWidth name="confirm_password" label="confirm Password"  type="password" id="confirm_password"  required/>
-            <InputMask mask="(+99)9999999999" >
-              {() => <TextField label="Your Mobile Number" fullWidth name='mobile_number' id='mobile_number'/>}
-            </InputMask>
-            <Box > <Link href="#" > Sent OTP </Link></Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row-reverse', marginTop: 1 }}> <Button type="submit" variant="contained"> Create Account </Button>
-            </Box>
+    };
 
 
 
+    return (
+        <div>
 
-          </Box>
-        </Box>
-
-      </Container>
-    </div>
+            <Container maxWidth="xs">
 
 
-  );
+                <Box sx={{ marginTop: 20, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
+
+
+                    <Box component="form" onSubmit={handleSubmit} >
+                        <TextField margin="normal" fullWidth label="Your First Name" name="first_name" required />
+                        <TextField margin="normal" fullWidth label="Your Last Name" name="last_name" required />
+                        <TextField margin="normal" fullWidth id="email" label="Your Email Address" name="email" required />
+                        <TextField margin="normal" fullWidth name="password" label="Your Password" type="password" id="password" required />
+                        <TextField margin="normal" fullWidth name="confirm_password" label="confirm Password" type="password" id="confirm_password" required />
+                        <InputMask mask="(+99)9999999999" >
+                            {() => <TextField label="Your Mobile Number" fullWidth name='mobile_number' id='mobile_number' />}
+                        </InputMask>
+                        <TextField margin="normal" fullWidth label="Your DOB" name="date_of_birth" required />
+                        <Box > <Link href="#" > Sent OTP </Link></Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'row-reverse', marginTop: 1 }}> <Button type="submit" variant="contained"> Create Account </Button>
+                        </Box>
+
+
+
+
+                    </Box>
+                </Box>
+
+            </Container>
+        </div>
+
+
+    );
 }
 export default Register
