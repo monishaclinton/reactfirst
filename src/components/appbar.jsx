256fd8a607136d4c5ac59b4f8fae0df89513df46
monishaclinton <monishajustinn97@gmail.com>
2023-08-30 19:43:47 -0700

    des

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -4,16 +4,59 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import { Typography,Grid } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
-
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 
 
 const Appbar = () => {
   var session_username = sessionStorage.getItem('sess_user_name')
   return (
     <div>
-    <Box height="40px" display="flex" align-items="center"  width="100%" sx={{ background: "#1f2c33" }} >
-
      
+
+    <Box height="40px" display="flex" align-items="center"  width="100%" sx={{ background: "#1f2c33" }} >
+      
+    <Box flex={1} pl="5px" pr="5px" >
+
+<Box
+  width="100%"
+  height="100%"
+  display="flex"
+  justifyContent="space-between"
+  alignItems="center"
+>
+  <Avatar />
+
+  <Box display="flex">
+    <IconButton
+      onClick={() => { }}
+      sx={{
+        paddingRight: "15px",
+      }}
+    >
+      <DonutLargeIcon
+        sx={{
+          color: "#afbac0",
+        }}
+      />
+    </IconButton>
+    <IconButton
+      onClick={() => { }}
+      sx={{
+        paddingRight: "10px",
+      }}
+    >
+      <ChatIcon
+        sx={{
+          color: "#afbac0",
+        }}
+      />
+    </IconButton>
+
+  </Box>
+</Box>
+
+</Box>
     
   
       <Box  width="100%"  height="100%" display="flex" justifyContent="space-between" alignItems="center">
